6ecb9fd8587692ff837244b29255c10c43987788
giuseppe <giuseppemumolo@gmail.com>
2019-07-24 17:48:53 +0200

    first commit

---
+++ ImageGallery/src/ImageGallery.Client/Startup.cs
@@ -0,0 +1,101 @@
+﻿using IdentityModel;
+using ImageGallery.Client.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth.Claims;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace ImageGallery.Client
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            
+            //clear the ClaimTypeMap to keep the original claim types
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+        }
+ 
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            // register an IHttpContextAccessor so we can access the current
+            // HttpContext in services by injecting it
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            // register an IImageGalleryHttpClient
+            services.AddScoped<IImageGalleryHttpClient, ImageGalleryHttpClient>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+            }).AddCookie("Cookies", options =>
+            {
+                options.AccessDeniedPath = "/Authorization/AccessDenied";
+            })
+            .AddOpenIdConnect("oidc", options =>
+            {
+                options.SignInScheme = "Cookies";
+                options.Authority = "https://localhost:44359";
+                options.ClientId = "imagegalleryclient";
+                options.ClientSecret = "secret";
+                //options.CallbackPath = new PathString("..."); //default = https://localhost:44356/signin-oidc
+                //options.SignedOutCallbackPath = new PathString("..."); //default = https://localhost:44356/signout-callback-oidc
+                options.ResponseType = "code id_token";
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                options.Scope.Add("address");
+                options.Scope.Add("roles");
+                options.Scope.Add("imagegalleryapi");
+                options.SaveTokens = true;
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.ClaimActions.Remove("amr"); //remove the claim action that filter out the amr claim
+                options.ClaimActions.DeleteClaim("sid"); //filter out the sid claim
+                options.ClaimActions.DeleteClaim("idp");
+                options.ClaimActions.MapUniqueJsonKey("role", "role");
+                
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = JwtClaimTypes.GivenName,
+                    RoleClaimType = JwtClaimTypes.Role
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Shared/Error");
+            }
+
+            app.UseAuthentication();
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Gallery}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ ImageGallery/src/MG_IdentityProvider/Config.cs
@@ -0,0 +1,107 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MG_IdentityProvider
+{
+    public static class Config
+    {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "d860efca-22d9-47fd-8249-791ba61b07c7",
+                    Username = "Giuseppe",
+                    Password = "password",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Giuseppe"),
+                        new Claim("family_name", "Underwood"),
+                        new Claim("address", "tree road 52"),
+                        new Claim("role", "FreeUser")
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "b7539694-97e7-4dfe-84da-b4256e1ff5c7",
+                    Username = "Giancarlo",
+                    Password = "password",
+                    
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Giancarlo"),
+                        new Claim("family_name", "Underwood"),
+                        new Claim("address", "tree road 42"),
+                        new Claim("role", "PayingUser")
+                    }
+                },
+            };
+        }
+
+        // identity-related resources (scopes)
+        public static List<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Address(),
+                new IdentityResource(
+                    "roles",            //scope name
+                    "Your role(s)",     //display name shown to the user when asking consent
+                    new[] { "role" })   //claim types tied to the scope
+            };
+        }
+
+        public static List<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource(
+                    "imagegalleryapi", // scope name 
+                    "Image Gallery API", // display name  
+                    new[] { "role" }) // claim types to add to the access token
+            };
+        }
+
+        public static List<Client> GetClients()
+        {
+            return new List<Client>()
+            {
+                new Client {
+                    ClientName = "Image Gallery",
+                    ClientId = "imagegalleryclient",
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    RedirectUris = new List<string>
+                    {
+                        "https://localhost:44356/signin-oidc"
+                    },
+                    PostLogoutRedirectUris =
+                    {
+                        "https://localhost:44356/signout-callback-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Address,
+                        "roles",
+                        "imagegalleryapi"
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    }
+                }
+            };
+        }
+    }
+}
