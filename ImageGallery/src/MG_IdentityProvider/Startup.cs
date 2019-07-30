10e28f3acde6f4bcf1b6e3fa6a36b81c9d04eb55
giuseppe <giuseppemumolo@gmail.com>
2019-07-30 12:03:13 +0200

    Added external authentication

--- ImageGallery/src/MG_IdentityProvider/Startup.cs
+++ ImageGallery/src/MG_IdentityProvider/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -22,6 +23,14 @@ namespace MG_IdentityProvider
                 .AddInMemoryIdentityResources(Config.GetIdentityResources())
                 .AddInMemoryApiResources(Config.GetApiResources())
                 .AddInMemoryClients(Config.GetClients());
+            services.AddAuthentication()
+                .AddGoogle("Google", options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme; //cookie handler configured by identityserver4 for external service
+
+                    options.ClientId = "272433830027-keh9juh59oq6k51dd0ved6a0jlaqslpv.apps.googleusercontent.com";
+                    options.ClientSecret = "MR4nmb_lL0lAK0SJ4HOOudYY";
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
